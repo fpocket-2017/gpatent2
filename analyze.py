8433f42df6fb4a77bad8d0ef68cb9c915a8bdbd9
Yoshihiro Shimizu <31213083+fpocket-2017@users.noreply.github.com>
2017-09-21 00:40:03 +0900

    commit

--- analyze.py
+++ analyze.py
@@ -2,6 +2,7 @@
 import os
 import codecs
 import re
+import pdb
 
 import xml.etree.ElementTree as et
 
@@ -11,27 +12,7 @@ import bs4
 from  const import PATH_DIR_OUTPUT_TEXT
 import utils
 
-def readSearchWord(fp):
-# If no search word exist, return "%all%"
-
-    text = fp.read()
-    sWords = []
-    if re.search('\r', text):
-        if re.search('\n', text):
-            sWords = re.split("\r\n", text)
-        else:
-            sWords = re.split("\r", text)
-    elif re.search('\n', text):
-        sWords = re.split("\n", text)
-    else:
-        sWords.append(text)
-
-    if sWords[len(sWords) - 1] == '':
-        del sWords[len(sWords) - 1]
-
-    return(sWords)
-
-def html_analyze(text):
+def htmlParse(text):
     soup = bs4.BeautifulSoup(text, 'lxml')
     section = soup.find(itemprop = u"description")
     if section != None:
@@ -39,42 +20,44 @@ def html_analyze(text):
     else:
         return(None)
 
-def paragraphs_split(text):
+def paragraphSplit(text):
     return(re.split(r"\n", text))
 
-def word_search(root, sWords, paragraphs):
+def wordSearch(root, search_words, paragraphs):
     for paragraph in paragraphs:
-        for word in sWords:
+        for word in search_words:
             if re.search(word, paragraph) != None:
                 p = et.SubElement(root, 'p')
                 p.text = paragraph
                 break
 
-def analyze(root, sWords, pnum, url):
+def analyze(root, search_words, pnum, url):
     fname = '{0}/{1}_{2}.txt'.format(PATH_DIR_OUTPUT_TEXT, pnum, 'd')
+
     if os.path.exists(fname):
-        print('Text data is already exist')
-        fp = codecs.open(fname, 'r', 'utf-8')
-        paragraphs = paragraphs_split(fp.read())
-        fp.close
+        print(u'Text data is already exist')
+        
+        with codecs.open(fname, 'r', 'utf-8') as fp:
+            text = fp.read()
     else:
         print('Getting Information from Patents.Google.com...')
+        
         response = requests.get(url)
-        print('Status Code: ' + str(response.status_code) + '\n')
+        print(u'Status Code: {0}\n'.format(str(response.status_code)))
 
         response.encoding = 'utf-8'
         
-        description = html_analyze(response.text)
+        description = htmlParse(response.text)
 
         if description == None:
             print('Description is not available\n')
             return()
         
-        fp = codecs.open(fname, 'w', 'utf-8')
-        fp.write(description.text)
-        fp.close
-        
-        paragraphs = paragraphs_split(description.text)
+        text = description.text
+        with codecs.open(fname, 'w', 'utf-8') as fp:
+            fp.write(text)
+
+    paragraphs = paragraphSplit(text)
 
     print('Analyzing...\n')
-    word_search(root, sWords, paragraphs)
+    wordSearch(root, search_words, paragraphs)
